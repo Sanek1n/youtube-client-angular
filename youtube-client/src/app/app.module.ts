9a51c42fff8d1786f388ce138009b1c0d58ecbc8
Sanek1n <sanekin8ru@outlook.com>
2023-03-15 14:12:47 +0400

    feat: generate all the necessary components, create necessary interfaces

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import AppComponent from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SearchSettingsComponent } from './settings/search-settings/search-settings.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { RegistrationComponent } from './main/registration/registration.component';
+import { CreateCardComponent } from './main/create-card/create-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchSettingsComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    RegistrationComponent,
+    CreateCardComponent,
   ],
   imports: [
     BrowserModule,
