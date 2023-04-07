336867255b9e3bed9ee029d217e7525a43459ce1
Sanek1n <sanekin8ru@outlook.com>
2023-04-07 14:39:24 +0400

    feat: add validation for admin page

--- youtube-client/src/app/youtube/components/create-card/create-card.component.ts
+++ youtube-client/src/app/youtube/components/create-card/create-card.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder, FormControl, FormGroup, ValidationErrors, Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import AuthService from '@app/auth/services/auth.service';
 
@@ -27,12 +29,17 @@ export default class CreateCardComponent {
 
   createForm(): void {
     this.cardForm = this.fb.group({
-      title: [this.newCard.title, [Validators.required]],
-      description: [this.newCard.description, [Validators.required]],
-      image: [this.newCard.image, [Validators.required]],
-      linkVideo: [this.newCard.linkVideo, [Validators.required]],
-      date: [this.newCard.linkVideo, [Validators.required]],
+      title: [this.newCard.title,
+        [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      description: [this.newCard.description, [Validators.maxLength(255)]],
+      image: [this.newCard.image, [Validators.required, this.urlValidator]],
+      linkVideo: [this.newCard.linkVideo, [Validators.required, this.urlValidator]],
+      date: [this.newCard.linkVideo, [Validators.required, this.dataValidator]],
     });
+  }
+
+  set setTitle(x: string) {
+    this.cardForm.setValue({ title: x });
   }
 
   get getTitle() {
@@ -55,10 +62,44 @@ export default class CreateCardComponent {
     return this.cardForm.get('date');
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  private dataValidator(control: FormControl): ValidationErrors | null {
+    const { value } = control;
+    const isDateWrong = (Date.now() - Date.parse(value) > 0 && Date.parse(value) > 0);
+
+    if (!isDateWrong) {
+      return { invalidDate: 'The date is invalid' };
+    }
+    return null;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private urlValidator(control: FormControl): ValidationErrors | null {
+    const { value } = control;
+
+    const isValidUrl = (url: string): boolean => {
+      try {
+        return Boolean(new URL(url));
+      } catch (e) {
+        return false;
+      }
+    };
+
+    if (!isValidUrl(value)) {
+      return { invalidUrl: 'The video link is invalid' };
+    }
+    return null;
+  }
+
   setIsSubmit() {
+    this.newCard = this.cardForm.value;
     if (this.cardForm.valid) {
-      console.log(this.newCard);
       this.router.navigateByUrl('');
+    } else {
+      Object.keys(this.cardForm.controls).forEach((field) => {
+        const control = this.cardForm.get(field);
+        control?.markAsTouched({ onlySelf: true });
+      });
     }
   }
 }
